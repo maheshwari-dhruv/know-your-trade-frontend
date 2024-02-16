5c4adebc5f7a37b026544d57539aefce000b356d
Dhruv Maheshwari <mrdm026@gmail.com>
2024-02-16 23:54:06 +0530

    working on navbar

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,5 +1,5 @@
 import { FooterSection } from "../components/footer-section";
-import { HeaderSection } from "../components/header-section";
+import { NavbarSection } from "../components/navbar-section";
 import { CategorySection } from "../components/home-page/category-section";
 import { LatestPostSection } from "../components/home-page/latest-post-section";
 import { MorePostSection } from "../components/home-page/more-post-section";
@@ -8,8 +8,8 @@ import { SubscribEmailSection } from "../components/home-page/subscribe-email";
 export const Home = () => {
   return (
     <div className="w-full h-full px-5 py-5 bg-zinc-900 main">
-      <div className="w-full h-full text-black border-2 rounded-md bg-white px-40 flex flex-col gap-4">
-        <HeaderSection />
+      <div className="flex flex-col w-full h-full gap-4 px-40 text-black bg-white border-2 rounded-md">
+        <NavbarSection />
         <LatestPostSection />
         <CategorySection />
         <MorePostSection />
