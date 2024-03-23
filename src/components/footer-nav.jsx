61c2c8e7a2ed183459c8fb737fab0d5e97bc2262
Dhruv Maheshwari <mrdm026@gmail.com>
2024-03-23 17:56:54 +0530

    updated footer

---
+++ src/components/footer-nav.jsx
@@ -0,0 +1,167 @@
+const LINKS = [
+  {
+    title: "Indicators",
+    items: [
+      "Moving Averages",
+      "RSI",
+      "Lagging Indicators",
+      "Leading Indicators",
+    ],
+  },
+  {
+    title: "Stocks",
+    items: ["52W High", "52W Low", "Breakout Stocks"],
+  },
+  {
+    title: "Team",
+    items: ["About us", "Contact"],
+  },
+  {
+    title: "Social",
+    items: ["Instagram", "Github", "Twitter"],
+  },
+];
+
+const FooterNav = () => {
+  return (
+    <div className="px-8 pt-8 bg-white rounded-lg">
+      <div className="flex justify-between">
+        <div className="w-1/3 text-left">
+          <a href="/" className="mb-3 font-sans text-2xl font-bold text-black">
+            KnowYourTrade
+          </a>
+          <p className="my-3 font-sans text-base">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+            optio. Dolore asperiores sunt amet ullam rem labore cumque delectus
+            illo sequi nam laudantium repudiandae, tenetur placeat accusamus
+            reiciendis ipsam corporis?
+          </p>
+          <div className="flex gap-4 mt-5">
+            <a href="#facebook" className="text-white">
+              <div className="px-4 py-2 bg-gray-700 rounded-md">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 8 19"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </a>
+            <a href="#twitter" className="text-white">
+              <div className="px-4 py-2 bg-gray-700 rounded-md">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 17"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </a>
+            <a href="#github" className="text-white">
+              <div className="px-4 py-2 bg-gray-700 rounded-md">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+            </a>
+          </div>
+          {/* <div className="flex items-center justify-start gap-5 border border-red-800">
+              <a href="#facebook" className="text-gray-700">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 8 19"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+              <a href="#twitter" className="text-gray-700">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 17"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+              <a href="#github" className="text-gray-700">
+                <svg
+                  className="w-5 h-5"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </a>
+            </div> */}
+        </div>
+        <div className="flex justify-end w-2/4 gap-8">
+          {LINKS.map(({ title, items }) => (
+            <div key={title} className="px-2 text-left">
+              <h2 className="mb-4 text-sm font-bold text-gray-900 uppercase">
+                {title}
+              </h2>
+              <ul>
+                {items.map((link) => (
+                  <li
+                    className="mb-4 text-gray-400 hover:text-gray-600"
+                    key={link}
+                  >
+                    <a href={`#${link}`}>{link}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="px-8 pt-4 font-bold leading-none text-center text-black text-9xl">
+        <span className="">KnowYourTrade</span>
+      </div>
+    </div>
+  );
+};
+
+export default FooterNav;
